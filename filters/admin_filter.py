ce4423c18da1c65311d8df33bbe0d6beb3ce7f43
yarchex <artur-popov-06@outlook.com>
2025-06-06 15:43:05 +0300

    Update admin_filter.py

--- filters/admin_filter.py
+++ filters/admin_filter.py
@@ -1,1 +1,7 @@
+from aiogram.dispatcher.filters import BoundFilter
+from aiogram import types
+from config.settings import settings
 
+class IsAdminFilter(BoundFilter):
+    async def check(self, message: types.Message):
+        return message.from_user.id in settings.ADMIN_IDS
