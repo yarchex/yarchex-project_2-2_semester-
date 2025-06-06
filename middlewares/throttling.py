e782465c0758fe45737f68abca28c1776cebd554
yarchex <artur-popov-06@outlook.com>
2025-06-06 15:42:41 +0300

    Update throttling.py

--- middlewares/throttling.py
+++ middlewares/throttling.py
@@ -1,1 +1,9 @@
+from aiogram import types
+from aiogram.dispatcher.middlewares import BaseMiddleware
+from utils.cache import cache
 
+class ThrottlingMiddleware(BaseMiddleware):
+    async def on_process_message(self, message: types.Message, data: dict):
+        if await cache.exists(f"throttle:{message.from_user.id}"):
+            raise CancelHandler()
+        await cache.set(f"throttle:{message.from_user.id}", "1", expire=2)
