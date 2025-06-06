7f257ce3e83a580b617c265f67cf23e39cff7406
yarchex <artur-popov-06@outlook.com>
2025-06-06 15:39:10 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,13 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+class Settings:
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    ALIPAY_API_KEY = os.getenv("ALIPAY_API_KEY")
+    RAKETA_API_KEY = os.getenv("RAKETA_API_KEY")
+    REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+    ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
+
+settings = Settings()
