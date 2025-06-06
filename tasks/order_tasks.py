d306e25a5155b9772fc62c9b9fc33f62f2715149
yarchex <artur-popov-06@outlook.com>
2025-06-06 15:51:40 +0300

    Update order_tasks.py

--- tasks/order_tasks.py
+++ tasks/order_tasks.py
@@ -1,1 +1,8 @@
+from celery import Celery
+from services.raketa_api import create_raketa_shipment
 
+app = Celery("tasks", broker="redis://localhost:6379/0")
+
+@app.task
+def process_order_async(order_data):
+    create_raketa_shipment(order_data)
