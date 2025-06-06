0392bc8014ce40887abdc28dbc58212454ce45e9
yarchex <artur-popov-06@outlook.com>
2025-06-06 15:43:42 +0300

    Update order_states.py

--- states/order_states.py
+++ states/order_states.py
@@ -1,1 +1,9 @@
+# states/order_states.py
+from aiogram.dispatcher.filters.state import StatesGroup, State
 
+class OrderStates(StatesGroup):
+    waiting_for_link = State()
+    waiting_for_size = State()
+    waiting_for_payment = State()
+    waiting_for_address = State()
+    confirmation = State()
