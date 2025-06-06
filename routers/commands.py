1f4c648884db3f40e8d3fe2c1704f801ad6f45de
yarchex <artur-popov-06@outlook.com>
2025-06-06 15:40:16 +0300

    Update commands.py

--- routers/commands.py
+++ routers/commands.py
@@ -1,1 +1,14 @@
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+from keyboards.inline import language_keyboard
 
+@dp.message_handler(commands=["start"])
+async def cmd_start(message: types.Message):
+    await message.answer(
+        "🛒 Бот для заказов из Poizon",
+        reply_markup=language_keyboard()
+    )
+
+@dp.message_handler(commands=["help"])
+async def cmd_help(message: types.Message):
+    await message.answer("Список команд: /new_order, /my_orders, /cancel")
